465aa2eb1b7610e6e9479a5af418e468b25ead8e
Johyunik <whgusdlr13@naver.com>
2023-12-19 09:54:51 +0900

    first commit

---
+++ src/main/java/com/example/hyunik_log/HyunikLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyunik_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunikLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyunikLogApplication.class, args);
+    }
+
+}
